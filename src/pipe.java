c4435925bca594dcd8d0ca5af4ab81fe1a156542
kdyung <kyung8888@gmail.com>
2014-04-23 00:30:48 -0700

    No good progress more changes made.

---
+++ src/pipe.java
@@ -0,0 +1,19 @@
+//This is a sample java script for running the pipeline
+
+//http://stackoverflow.com/questions/13991007/execute-external-program-in-java
+
+//Writing a simple java applet: http://www.cs.colorado.edu/~main/applets/Simple.html
+
+import java.applet.Applet; // Provides the Applet class.
+import java.awt.*;         // Provides Button class, etc.
+import java.awt.event.*;   // Provides ActionEvent, ActionListener 
+
+
+public class pipe extends Applet{
+    public void init( )
+    {
+        System.out.println("printed Java");
+    }
+}
+
+
